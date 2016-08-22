76e441939e9eac1084a487de351793be48742046
yogiprayogo <yogi.prayogo@go-jek.com>
2016-08-22 23:13:51 +0700

    1st code refactor

--- Noughts And Crossess/ViewController.swift
+++ Noughts And Crossess/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var playAgainButton: UIButton!
     
     var activePlayer = 1 //1 equal noughts & 2 equal crossess
-    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var gameState = [Int]()
     let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     var gameActive = true
     
@@ -23,10 +23,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        gameOverLabel.hidden = true
-        gameOverLabel.center = CGPointMake(gameOverLabel.center.x - 500, gameOverLabel.center.y)
-        
-        playAgainButton.hidden = true
+        startGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,52 +45,7 @@ class ViewController: UIViewController {
                 activePlayer = 1
             }
             
-            for combination in winningCombinations {
-                
-                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
-                    
-                    gameActive = false
-                    
-                    if gameState[combination[0]] == 1 {
-                        
-                        //print("Noughts have won!")
-                        gameOverLabel.text = "Noughts have won!"
-                        
-                    } else {
-                        
-                        //print("Crosses have won!")
-                        gameOverLabel.text = "Crosses have won!"
-                        
-                    }
-                    
-                    endGame()
-                    
-                }
-                
-            }
-            
-            if gameActive == true {
-            
-                gameActive = false
-            
-                for buttonState in gameState {
-                
-                    if buttonState == 0 {
-                    
-                        gameActive = true
-                    
-                    }
-                
-                }
-            
-                if gameActive == false {
-                
-                    gameOverLabel.text = "It's a draw!"
-                    endGame()
-                
-                }
-                
-            }
+            checkResult()
             
         }
         
@@ -101,16 +53,7 @@ class ViewController: UIViewController {
     
     @IBAction func playAgain(sender: AnyObject) {
         
-        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        
-        activePlayer = 1
-        
-        gameActive = true
-        
-        gameOverLabel.hidden = true
-        gameOverLabel.center = CGPointMake(gameOverLabel.center.x - 500, gameOverLabel.center.y)
-        
-        playAgainButton.hidden = true
+        startGame()
         
         var buttonToClear : UIButton
         
@@ -123,6 +66,22 @@ class ViewController: UIViewController {
         
     }
     
+    func startGame() {
+        
+        //set var
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        activePlayer = 1
+        gameActive = true
+        
+        
+        //prepare the UI
+        gameOverLabel.hidden = true
+        gameOverLabel.center = CGPointMake(gameOverLabel.center.x - 500, gameOverLabel.center.y)
+        
+        playAgainButton.hidden = true
+        
+    }
+    
     func endGame() {
         
         playAgainButton.hidden = false
@@ -130,6 +89,54 @@ class ViewController: UIViewController {
         UIView.animateWithDuration(0.5, animations: {
             self.gameOverLabel.center = CGPointMake(self.gameOverLabel.center.x + 500, self.gameOverLabel.center.y)
         })
+        
+    }
+    
+    func checkResult() {
+        
+        //see who's the winner
+        for combination in winningCombinations {
+            
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+                
+                gameActive = false
+                
+                if gameState[combination[0]] == 1 {
+                    
+                    //print("Noughts have won!")
+                    gameOverLabel.text = "Noughts have won!"
+                    
+                } else {
+                    
+                    //print("Crosses have won!")
+                    gameOverLabel.text = "Crosses have won!"
+                    
+                }
+                
+                endGame()
+                
+            }
+            
+        }
+        
+        //is it draw?
+        if gameActive == true {
+            
+            gameActive = false
+            for buttonState in gameState {
+                
+                if buttonState == 0 {
+                    gameActive = true
+                }
+                
+            }
+            
+            if gameActive == false {
+                gameOverLabel.text = "It's a draw!"
+                endGame()
+            }
+            
+        }
         
     }
     
